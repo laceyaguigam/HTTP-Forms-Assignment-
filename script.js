347e86583014b7cb095cfe31e86569e545b86060
lacey aguigam <laceyaguigam@yahoo.com>
2023-12-02 20:08:57 -0800

    intial commit and added comments for instructions

---
+++ script.js
@@ -0,0 +1,34 @@
+// 1. Write code to allow visitors of the page to customize it to their liking. There is a
+// form on the page that accepts a name (to be used in a greeting when the user visits
+// the page) and a color picker to allow the user to choose their preferred background
+// color/foreground color combination. 
+
+//Write the necessary code to capture these values
+// when the form is submitted (prevent the default action on the form submission button to
+// achieve this) and store these values in localStorage (so that it persists on the user’s
+// computer and their preferences are saved indefinitely).
+
+//Next, write a function to apply
+// the preferences if they have been set and call it each time the page loads. You may
+// also want to call this function again when the user saves their preferences to
+// immediately apply them. Make sure you also notify the user somehow that the preferences
+// were saved.
+
+
+
+//greet users, when user visits page
+
+//color picker to choose background 
+
+//color picker to choose foreground
+
+
+//when for submitted, prevent default 
+
+//store info in local storage 
+
+//write function to apply preferences 
+
+//call function again when user saves preferences and apply them 
+
+//notify user when preferences are saved
