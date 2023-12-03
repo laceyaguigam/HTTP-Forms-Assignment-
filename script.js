9f651d4b8f2fd296ea62f7588a43ceed196b8a2b
lacey aguigam <laceyaguigam@yahoo.com>
2023-12-02 22:01:14 -0800

    added comment to console.log for the submit button

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ let textInput = document.getElementById("name");
 let form = document.querySelector("form");
 form.addEventListener("submit", event => {
     event.preventDefault();
-    console.log("textInput.value"); //add the value for the text input
+    console.log("textInput.value"); //works but is it supposed to produce the value of the name that the user input? 
 });
 
     //id="submit" for submit button 
