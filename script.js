2686d599ddcf317fceaa0dc47aba6c44e6a1d67b
lacey aguigam <laceyaguigam@yahoo.com>
2023-12-12 15:11:10 -0800

    removed unnecessary comments and code, however, it dtill does not work and will need to be worked on

--- script.js
+++ script.js
@@ -1,81 +1,44 @@
-// 1. Write code to allow visitors of the page to customize it to their liking. There is a
-// form on the page that accepts a name (to be used in a greeting when the user visits
-// the page) and a color picker to allow the user to choose their preferred background
-// color/foreground color combination. 
-
-//Write the necessary code to capture these values
-// when the form is submitted (prevent the default action on the form submission button to
-// achieve this) and store these values in localStorage (so that it persists on the user’s
-// computer and their preferences are saved indefinitely).
-
-//Next, write a function to apply
-// the preferences if they have been set and call it each time the page loads. You may
-// also want to call this function again when the user saves their preferences to
-// immediately apply them. Make sure you also notify the user somehow that the preferences
-// were saved.
-
-
-
-//greet users, when user visits page
-    //id= "greeting" for the greeting at the top of the page?
-   
-// let greeting = document.getElementById("greeting");   //doesnt work 
-// greeting.innerText += localStorage.getItem("name");
-
-
-    //id="name" for the text input field
-
-// let textInput = document.getElementById("name");   
-
-
-
-
-                    //THIS WORKS!!!!!!!
-
-
-//color picker to choose background 
-    //id="background-color" for the background color picker 
-// let bg = document.getElementById("background-color");
-// bg.addEventListener('input', () => {
-//    document.body.style.backgroundColor = bg.value 
-//     localStorage.setItem("background-color"); //not sure if this is right 
-// });
-
-
-
-                //DOES NOT WORK!!!
-
-
-//color picker to choose foreground
-    //id="foreground-color" for the foreground picker
-//  let fg = document.getElementById("foreground-color");
-//     fg.addEventListener('input', () => {
-//      document.style.color = fg.value
-//  });
-
+  // doesn't work need to fix
 const name = document.getElementById('name');
 const background = document.getElementById('background-color');
 const foreground = document.getElementById('foreground-color');
 const body = document.querySelector('body');
+const greeting = getElementById('greeting');
 
 
-                    // THIS WORKS!!!!!!
+//background
+
+background.addEventListener('input', () => {
+   document.body.style.background = background.value 
+    localStorage.setItem("background-color"); //not sure if this is right 
+});
 
 
-//when form submitted, prevent default 
- //id="submit" for submit button 
+
+//foreground
+   
+    foreground.addEventListener('input', () => {
+     document.body.style.color = foreground.value
+ });
+
+
+
+
+//submits form and gives alert
 let form = document.querySelector("form");
 form.addEventListener("submit", event => {
     event.preventDefault();
     console.log("name: " + name.value);
     console.log("background: " + background.value);
     console.log("foreground: " + foreground.value); 
-    alert("Your preferences have been saved!"); //alerts user that preferences were saved when form submitted
+    window.alert("Your preferences have been saved!"); //alerts user that preferences were saved when form submitted
+    applyPreference();
+
 
     localStorage.setItem('name', name.value);    //(key (how you name your variables), value (the value variable given))
     localStorage.setItem('background', background.value);  
     localStorage.setItem('foreground', foreground.value);  
-    applyPreference();
+    
 });
 
    
@@ -91,17 +54,21 @@ function applyPreference() {
     name.value = localStorage.getItem("name");
     background.value = localStorage.getItem("background");
     foreground.value = localStorage.getItem("foreground");
-}
+};
 
 window.onload = (event)  => {
     applyPreference();
-}
+};
+
+
+
+
+
+
 
 //in class notes 
 
 // const name =document.getElementById('name');
-// const bg = document.getElementById('background-color');
-// const fg = document.getElementById('foreground-color');
+
 
 // //greeting
-// const greeting = getElementById('greeeting');
